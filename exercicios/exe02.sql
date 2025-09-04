6fa1390f425d6ea27b5e68999b0b37957d29953a
mathfnz_macbook <matheus.fnds20@gmail.com>
2025-09-04 18:31:05 -0300

    add exercises

---
+++ exercicios/exe02.sql
@@ -0,0 +1,7 @@
+-- Selecione toda as transações de = 50
+SELECT
+    *
+FROM
+    transacoes
+WHERE
+    QtdePontos = 50
---
+++ exercicios/exe03.sql
@@ -0,0 +1,8 @@
+-- Selecione todos os clientes com mais de 500 pontos
+SELECT
+    IdCliente,
+    QtdePontos
+FROM
+    clientes
+WHERE
+    QtdePontos > 500
